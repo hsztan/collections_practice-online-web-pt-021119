00b026816c41a5358a8ce8c39f148cf469854c87
Henry Sztanski <henry@sztanski.com>
2019-02-19 20:19:46 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+require "pry"
 
 def sort_array_asc(array)
  array.sort
@@ -5,4 +6,15 @@ end
 
 def sort_array_desc(array)
   array.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  holder = array[1]
+  array[1] = array[2]
+  array[2] = holder
+  array
 end
